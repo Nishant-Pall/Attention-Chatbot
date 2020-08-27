8a6156ac264c6e4ce29ab0c975f1f7c72e9aa681
nishant <palln6935@gmail.com>
2020-08-27 13:45:00 +0530

    Create questions and answers, and clean the text

---
+++ ChatBot.py
@@ -0,0 +1,68 @@
+# Importing the libraries
+import numpy as np
+import re
+import tensorflow as tf
+import time
+
+################## DATA PREPROCESSING ##################
+
+# Importing Dataset
+lines = open('movie_lines.txt', encoding='utf-8',
+             errors='ignore').read().split('\n')
+conversations = open('movie_conversations.txt', encoding='utf-8',
+                     errors='ignore').read().split('\n')
+
+# Creating a dictionary that maps each line with its id
+id2line = {}
+
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+
+# Creating a list of all of the conversations
+conversations_ids = []
+for conversation in conversations[:-1]:
+    _conversation = conversation.split(
+        ' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
+    conversations_ids.append(_conversation.split(','))
+
+# Getting separately the questions and answers
+questions = []
+answers = []
+for conversation in conversations_ids:
+    for i in range(len(conversation) - 1):
+        questions.append(id2line[conversation[i]])
+        answers.append(id2line[conversation[i+1]])
+
+# Doing the first cleaning of the texts
+
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "can not", text)
+    text = re.sub(r"don't", "do not", text)
+    text = re.sub(r"[-()\"#/@:;+=~<>{}?,|]", "", text)
+    return text
+
+
+# Cleaning the questions
+clean_questions = []
+for question in questions:
+    clean_questions.append(clean_text(question))
+
+# Cleaning the answers
+clean_answers = []
+for answer in answers:
+    clean_answers.append(clean_text(answer))
